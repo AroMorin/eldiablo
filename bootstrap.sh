c70ab96195c220dc5c3fc3d10713c0e27f54e302
System Administrator <root@AhmedMAC.local>
2015-06-16 00:43:17 -0400

    changed bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' |
-tee /etc/apt/sources.list.d/mongodb.list
+tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 
 sudo apt-get update
 
@@ -63,7 +63,7 @@ cd
 sudo mv nltk_data /usr/lib/nltk_data
 
 echo "Installing MongoDB..."
-sudo apt-get install mongodb-10gen
+sudo apt-get install -y mongodb-org
 
 echo "Setting up crontab..."
-sudo crontab /vagrant/crontab.txt
+sudo crontab crontab.txt
